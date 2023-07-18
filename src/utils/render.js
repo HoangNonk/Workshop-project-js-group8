9155f0a95d0df3088cdb12d5ed034c4f834d79cf
AnhVanHoa <anhhoccode@gmail.com>
2023-07-18 08:01:50 +0700

    fix useEffect

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,8 +1,10 @@
+import { effects } from './useEffect';
 import Navigo from 'navigo';
 const router = new Navigo('/');
 const app = document.querySelector('#app');
 function render(page) {
     app.innerHTML = page;
+    effects.forEach((element) => element.cb());
 }
 
 export default render;
