569eee2069625a7b387589bf477175ff893053b8
AnhVanHoa <anhhoccode@gmail.com>
2023-07-20 10:38:04 +0700

    fix config

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import home from '~/pages/client/home';
 import homeAdmin from '~/pages/admin/home';
 const routersClient = [
@@ -19,30 +18,3 @@ const routersAdmin = [
 
 export default routersClient;
 export { routersAdmin };
-=======
-import home from '~/pages/client/home';
-import homeAdmin from '~/pages/admin/home';
-const routersClient = [
-    {
-        path: '/',
-        element: home,
-        layout: '',
-    },
-];
-
-const routersAdmin = [
-    {
-        path: '/admin',
-        element: homeAdmin,
-        layout: '',
-    },
-    {
-        path: '/admin/user',
-        element: homeAdmin,
-        layout: '',
-    },
-];
-
-export default routersClient;
-export { routersAdmin };
->>>>>>> 09e4c7aedf1d5a85118ccaeec1510218a5e38729
